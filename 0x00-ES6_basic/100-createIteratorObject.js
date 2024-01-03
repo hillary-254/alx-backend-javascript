af8995a4701ea36ff17d0508f1db5bad54d3addb
hillary-254 <hillarykim001@gmail.com>
2024-01-03 16:38:45 +0300

    fixed code to align with ESLint rules

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,10 @@
-import createIteratorObject from "./100-createIteratorObject.js";
-import createEmployeesObject from './11-createEmployeesObject.js';
-import createReportObject from './12-createReportObject.js';
+import createIteratorObject from './100-createIteratorObject';
+import createEmployeesObject from './11-createEmployeesObject';
+import createReportObject from './12-createReportObject';
 
 const employees = {
-    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
-    ...createEmployeesObject('marketing', ['Sylvie'])
+  ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+  ...createEmployeesObject('marketing', ['Sylvie'])
 };
 
 const report = createReportObject(employees);
@@ -12,5 +12,5 @@ const report = createReportObject(employees);
 const reportWithIterator = createIteratorObject(report);
 
 for (const item of reportWithIterator) {
-    console.log(item);
+  console.log(item);
 }
