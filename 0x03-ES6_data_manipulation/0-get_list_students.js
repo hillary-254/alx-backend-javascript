17195002347640d9d830e216873ac7a4cedfaae9
hillary-254 <hillarykim001@gmail.com>
2024-01-08 19:18:52 +0300

    0. Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+/**
+ * Returns an array of objects representing students.
+ * @returns {Array} Array of student objects.
+ */
+export default function getListStudents() {
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ]
+  return students
+}
