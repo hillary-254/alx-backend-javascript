3dd2beedd0c7b6d1e99b46c9c1d499bf278bf435
hillary-254 <hillarykim001@gmail.com>
2024-01-08 19:19:23 +0300

    2. Filter

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,13 @@
+/**
+ * Returns an array of students located in a specific city.
+ * @param {Array} studentsArray - Array of student objects.
+ * @param {string} city - The city to filter students by.
+ * @returns {Array} Array of student objects in the specified city.
+ */
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array){
+    const studentLocation = students.filter((student) => student.location === city)
+    return studentLocation
+  }
+  return []
+}
