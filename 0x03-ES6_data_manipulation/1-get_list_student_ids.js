52c82b5811c7aeacce37af769335d7be7fef8f36
hillary-254 <hillarykim001@gmail.com>
2024-01-08 19:41:32 +0300

    fixing

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,10 @@
 /**
  * Returns an array of student ids from a list of objects.
- * @param {Array} studentsArray - Array of student objects.
+ * @param {{
+*   id: Number,
+*   firstName: String,
+*   location: String
+* }[]} students - The list of students.
  * @returns {Array} Array of student ids.
  */
 export default function getListStudentIds(students) {
